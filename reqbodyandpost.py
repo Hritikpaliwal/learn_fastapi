1c04505c98d0b636b79c57b3fbfd8f1ac6d7fbd7
hritikpaliwal <phritik20@gmail.com>
2024-03-27 02:38:25 +0530

    learned req body and post parameters

--- reqbodyandpost.py
+++ reqbodyandpost.py
@@ -20,12 +20,20 @@ inventory = {
 }
 
 @app.get("/get_item/{item_id}")
-def get_item(item_id: int = Path (None, description = "The ID of the item you would wana pull", gt=0, lt=2)):
+def get_item(item_id: int = Path ( description = "The ID of the item you would wana pull")):
     return inventory[item_id]
 
 @app.get("/get-by-name")
-def get_item(name: str):
+def get_item(*, item_id: int,name: Optional[str] = None ):
     for item_id in inventory:
-        if inventory[item_id]["name"]== name:
+        if inventory[item_id]["name"] == name:
             return inventory[item_id]
-    return{"Data": "not found"}
+    return{"Data": "Not Found"}
+
+@app.post("/create-item")
+def create_item(item_id: int,item: Item):
+    if item_id in inventory:
+      return {"Error": "Item already exists."}\
+    
+    inventory[item_id] = {"name": item.name, "brand": item.brand, "price": item.price}
+    return inventory[item_id]
